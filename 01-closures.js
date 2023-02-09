9054945b978aeee7fed3bd6125315fbe94d4fc35
Bridgette Lemus <bridgette.lemus.se@gmail.com>
2023-02-09 09:59:26 -0500

    coding questions

---
+++ 01-closures.js
@@ -0,0 +1,6 @@
+/***
+ * What is Closure?
+ * A closure is the coombination of a function bundled together (enclosed) with references to it's surrounding state (the lexical environment)
+ * In other words, a closure gives you access to an outer function's scope from an inner function. In JavaScript. closures are created every time a function is created, at function creation time
+ * 
+ * ********/  
