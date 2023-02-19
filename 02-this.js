2cdbe28c410ff220edcb40ff8ec75645970a6c5a
Bridgette Lemus <bridgette.lemus.se@gmail.com>
2023-02-19 16:11:13 -0500

    defintition and interpretation of this in javascript

---
+++ 02-this.js
@@ -0,0 +1,10 @@
+/**
+ * what is "This", the reserved keyword in Javascript?
+ * - The value of this is determined by how the function is called (runtime binding)
+ * -It can't be set by assignment during execution, and it may different each time the function is called.
+ * - Arrow functions don't provide their own "this" binding
+ * -Reference: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/this
+ * 
+ * 
+ * 
+ */
